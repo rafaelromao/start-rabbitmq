ea03a96ed8c5cd45cac924fd94d51d13d1ade181
Rafael Romão <rafael.romao@concert.com.br>
2015-07-21 09:52:16 -0300

    Changed host messages to identify the image and the container

--- Start-RabbitMQ.psm1
+++ Start-RabbitMQ.psm1
@@ -6,24 +6,24 @@ function Start-RabbitMQ() {
 	# Check if the image exists
 	$imageExists = docker images | grep rabbitmq | grep rstests
 	if ($containerId -eq $null) {
-		Write-Host "Creating image..."
+		Write-Host "Creating RabbitMQ image..."
 		docker build --rm=true -t rabbitmq:rstests $PSScriptRoot 2> $null | Out-Null
 	}
 	# Check if container exists
 	$existingContainerId = docker ps -a -q --filter="name=rabbitmq-rstests"
 	if ($existingContainerId -eq $null) {
 		# Creates and starts the container
-		Write-Host "Creating and starting container..."
+		Write-Host "Creating and starting RabbitMQ container..."
 		docker run --name=rabbitmq-rstests -d -p 5672:5672 -p 15672:15672 rabbitmq:rstests 2> $null | Out-Null
 	} else {
 		$runningContainerId = docker ps -q --filter="name=rabbitmq-rstests"
 		if ($runningContainerId -ne $null) {
 			# Stop the existing container
-			Write-Host "Stopping container..."
+			Write-Host "Stopping RabbitMQ container..."
 			docker stop $runningContainerId
 		}
 		# Starts the existing container
-		Write-Host "Starting container..."
+		Write-Host "Starting RabbitMQ container..."
 		docker start $existingContainerId
 	}
 }
